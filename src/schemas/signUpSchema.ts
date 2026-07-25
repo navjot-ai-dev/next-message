8b83abbd649c95888d2160d982e9afcc7d1d3ac8
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-07-25 21:04:28 +0530

    complete schemas

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,7 @@
+import {z} from "zod";
+
+export const signUpSchema = z.object({  
+    username: z.string().min(2).max(100).regex(/^[a-zA-Z0-9_]+$/, 'Username can only contain letters, numbers, and underscores'),
+    email: z.string().email({message: 'Invalid email address'}),
+    password: z.string().min(6).max(100)
+});
---
+++ src/schemas/verifySchema.ts
@@ -0,0 +1,6 @@
+import {z} from "zod";
+
+export const verifySchema = z.object({
+    
+    verifyCode: z.string().length(6, {message: 'Verify code must be 6 characters long'})
+});
