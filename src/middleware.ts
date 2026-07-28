2756fd6abebd476dfebbaefacc8b5da57530174b
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-07-28 18:20:50 +0530

    complete middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import {NextRequest, NextResponse} from 'next/server';
-import type {NextRequest} from 'next/server';
+
 export {default} from 'next-auth/middleware';
 import { getToken } from "next-auth/jwt";
 import { Request } from 'next/dist/compiled/@edge-runtime/primitives';
@@ -9,8 +9,20 @@ export async function middleware(request: NextRequest) {
 
     const token = await getToken({req: request })
     const url = request.nextUrl
+
+    if (token && 
+        (
+            url.pathname.startsWith('/sign-in') ||
+            url.pathname.startsWith('/sign-up') ||
+            url.pathname.startsWith('/verify')||
+            url.pathname.startsWith('/')
+        )
+
+    ) {
+        return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
     
-    return NextResponse.redirect(new URL('/login', req.url));
+    return NextResponse.redirect(new URL('/home', request.url));
 }
 
 export const config = {
