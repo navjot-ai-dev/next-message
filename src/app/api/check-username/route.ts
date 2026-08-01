d0fc4ba7d08f45267111e6f249f0f93481f5114a
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-08-01 15:14:37 +0530

    add code in check username

--- src/app/api/check-username/route.ts
+++ src/app/api/check-username/route.ts
@@ -20,7 +20,37 @@ export async function GET(request:Request) {
 
         //validate with zod
         const result = usernameValidation.safeParse(queryParam)
-        console.log(result)
+      if(!result.success){
+
+        const usernameErrors = result.error.format().username?._errors || []
+        return Response.json({
+            success: false,
+            message: usernameErrors.length > 0 ? usernameErrors.join(", ") : "Invalid username",
+        },
+    {status:400}
+    )
+      }
+
+      const {username} = result.data
+
+      const user = await UserModel.findOne({username,isVerified:true})
+
+      if(user){
+        return Response.json({
+            success: false,
+            message: "Username is already taken",
+        },
+    {status:400}
+    )
+      }
+
+        return Response.json({
+            success: true,
+            message: "Username is available",
+        },
+    {status:200}
+    )
+        
         
     } catch (error) {
         console.error("Error checking username", error);
