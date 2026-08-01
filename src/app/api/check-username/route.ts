1c77a0cc47645e75d3bc9dfcf8ee0032d39ed5b9
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-08-01 15:48:23 +0530

    complete check-username

--- src/app/api/check-username/route.ts
+++ src/app/api/check-username/route.ts
@@ -9,6 +9,9 @@ const usernameValidation = z.object({
 });
 
 export async function GET(request:Request) {
+   
+
+
     await dbConnect()
 
     try {
@@ -50,7 +53,7 @@ export async function GET(request:Request) {
         },
     {status:200}
     )
-        
+
         
     } catch (error) {
         console.error("Error checking username", error);
