66e5b5cd47dfaadd44e527c633db1df7b408f812
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-08-07 15:03:49 +0530

    add usehook ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import Link from 'next/link'
+import { useState } from 'react';
+import { useDebounceValue } from 'usehooks-ts'
 
 const page = () => {
+  const [username, setusername] = useState('');
+  const [usernameMessage, setusernameMessage] = useState('')
+  const [isCheckingUsername, setisCheckingUsername] = useState(false)
+  const [isSubmitting, setisSubmitting] = useState(false )
   return (
     <div></div>
   )
