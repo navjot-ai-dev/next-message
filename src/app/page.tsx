8995584b6a5c23c8d8bd7b9f8f77a4885d4e8a13
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-08-09 13:25:16 +0530

    delete form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,13 +66,35 @@ const page = () => {
           description: response.data.message
         });
         router.replace(`/verify/${username}`)
+        setisSubmitting(false)
       } catch (error) {
-        
+        console.error("error in signup of user", error);
+       const axiosError = error as AxiosError<ApiResponse>
+          
+          let errorMessage =  axiosError.response?.data.message 
+          toast.add({
+            title: "Signup failed",
+            type: "error",
+            description: errorMessage ?? 'Error signing up',
+            
+           
+          });
+          setisSubmitting(false)
+          
       }
     }
 
   return (
-    <div></div>
+    <div className='flex justify-center items-center min-h-screen
+    bg-gray-100'>
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className='text-center'>
+          <h1 className='text-4xl font-extrabold'>Join Mystery Message</h1>
+          <p className='mb-4'>Sign up to start sending anonymous messages </p>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
